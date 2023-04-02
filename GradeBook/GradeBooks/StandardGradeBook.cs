c8a823b0e8ffb7462a633a8f717c303fd4837807
knizinsky <108873272+knizinsky@users.noreply.github.com>
2023-04-02 13:26:17 +0200

    Created the StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
