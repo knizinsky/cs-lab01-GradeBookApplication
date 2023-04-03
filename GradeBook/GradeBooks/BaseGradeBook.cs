3e0af8e84257e38da61e50ca76920fd66f280e5b
knizinsky <108873272+knizinsky@users.noreply.github.com>
2023-04-03 22:00:17 +0200

    Updated StartingUserInterface's CreateCommand method

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,16 +9,19 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
         public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,35 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            var type = parts[2];
+            bool isWeighted = Convert.ToBoolean(parts[3]);
+
+            if (type == "standard")
+            {
+                StandardGradeBook gradebook = new StandardGradeBook(name, isWeighted);
+
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradebook);
+            }
+            else if (type == "ranked")
+            {
+                RankedGradeBook gradebook = new RankedGradeBook(name, isWeighted);
+
+                Console.WriteLine("Created gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(gradebook);
+            }
+            else
+            {
+                Console.WriteLine(parts[2] + " is not a supported type of gradebook, please try again");
+                return;
+            }
+
         }
 
         public static void LoadCommand(string command)
