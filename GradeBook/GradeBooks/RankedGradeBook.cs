083b7413da654e27ea7f2d4f90e7c4c7c51803a9
knizinsky <108873272+knizinsky@users.noreply.github.com>
2023-04-03 21:21:18 +0200

    Overrided RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,11 +7,45 @@ using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var twentyPercent = (int)Math.Ceiling(Students.Count * 0.2);
+            var sortedStudents = Students.OrderByDescending(s => s.AverageGrade).ToList();
+
+            var index = sortedStudents.FindIndex(s => s.AverageGrade == averageGrade);
+
+            if (index < twentyPercent)
+            {
+                return 'A';
+            }
+            else if (index < twentyPercent * 2)
+            {
+                return 'B';
+            }
+            else if (index < twentyPercent * 3)
+            {
+                return 'C';
+            }
+            else if (index < twentyPercent * 4)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
