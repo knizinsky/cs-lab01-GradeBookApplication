a94b8896c47f5121fd669cd4074ecde3da893187
knizinsky <108873272+knizinsky@users.noreply.github.com>
2023-04-04 13:55:13 +0200

    Revert "Test"

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,18 +1,19 @@
-﻿using GradeBook.Enums;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
 
 namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name, bool weighted) : base(name, weighted)
+        public StandardGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Standard;
         }
-
     }
 }
